e94bfa65bc95edd62bfc17e1c950609719fbbe76
DutchGhost <kasper199914@gmail.com>
2018-01-08 20:50:34 +0100

    added iterator versions of conversion

--- src/lib.rs
+++ src/lib.rs
@@ -191,33 +191,81 @@ where
 /// #Examples
 /// ```
 /// extern crate libaoc;
-/// use libaoc::{StrToNum};
+/// use libaoc::{TryConvert};
 ///
 /// fn main () {
 ///     let s = "1, 2, 3, 4, 5";
-///     assert_eq!(vec![1, 2, 3, 4, 5], s.split(", ").to_num().unwrap());
+///     assert_eq!(vec![1, 2, 3, 4, 5], s.split(", ").try_convert().unwrap());
 ///
 ///     let s = String::from("1\n2\n3\n4\n5\n6");
-///     assert_eq!(vec![1, 2, 3, 4, 5, 6], s.lines().to_num().unwrap());
+///     assert_eq!(vec![1, 2, 3, 4, 5, 6], s.lines().try_convert().unwrap());
+///
+///     let my_str = "1, 2, 3,4, 5";
+///     let mut iter = my_str.split(", ").try_convert_iter();
+///
+///     assert_eq!(Some(Ok(1)), iter.next());
+///     assert_eq!(Some(Ok(2)), iter.next());
+///     assert_ne!(Some(Ok(3)), iter.next());
+///     assert_eq!(Some(Ok(5)), iter.next());
 /// }
 /// ```
-pub trait StrToNum<N, S, I>
+pub trait TryConvert<N, S, I>
 where
     N: FromStr,
     S: AsRef<str>,
     I: Iterator<Item = S>,
 {
-    fn to_num(self) -> Result<Vec<N>, <N as FromStr>::Err>;
+    /// On succes, returns a vector of all completed conversions. When an error occures, returns an error instead.
+    fn try_convert(self) -> Result<Vec<N>, <N as FromStr>::Err>;
+
+    /// On succes, returns a vector of all completed conversions.
+    /// #Panic
+    /// Panics when an error occures.
+    unsafe fn unsafe_convert(self) -> Vec<N>
+    where
+        <N as FromStr>::Err: fmt::Debug;
+    
+    /// Returns an iterator over the converted items. Returns an error if an item can not be converted. Continue's after the error.
+    fn try_convert_iter(self) -> std::iter::Map<I, fn(S) -> Result<N, <N as FromStr>::Err>>;
+
+    /// Returns an iterator over the converted items.
+    /// #Panic
+    /// Panics when an error occures.
+    unsafe fn unsafe_convert_iter(self) -> std::iter::Map<I, fn(S) -> N>
+    where
+        <N as FromStr>::Err: fmt::Debug;
 }
 
-impl<N, S, I> StrToNum<N, S, I> for I
+impl<N, S, I> TryConvert<N, S, I> for I
 where
     N: FromStr,
     S: AsRef<str>,
     I: Iterator<Item = S>,
-{
-    fn to_num(self) -> Result<Vec<N>, <N as FromStr>::Err> {
-        self.map(|item| item.as_ref().parse()).collect()
+{   
+    #[inline]
+    fn try_convert(self) -> Result<Vec<N>, <N as FromStr>::Err> {
+        self.try_convert_iter().collect()
+    }
+
+    #[inline]
+    unsafe fn unsafe_convert(self) -> Vec<N>
+    where
+        <N as FromStr>::Err: fmt::Debug,
+    {
+        self.unsafe_convert_iter().collect()
+    }
+
+    #[inline]
+    fn try_convert_iter(self) -> std::iter::Map<I, fn(S) -> Result<N, <N as FromStr>::Err>> {
+        self.map(|item| item.as_ref().parse())
+    }
+
+    #[inline]
+    unsafe fn unsafe_convert_iter(self) -> std::iter::Map<I, fn(S) -> N>
+    where
+        <N as FromStr>::Err: fmt::Debug
+    {
+        self.map(|item| item.as_ref().parse().unwrap())
     }
 }
 
@@ -225,7 +273,7 @@ where
 /// #Examples
 /// ```
 /// extern crate libaoc;
-/// use libaoc::{ToVec, Position};
+/// use libaoc::{Convert, Position};
 /// fn main() {
 ///     let tuple1 = (0, 0);
 ///     let tuple2 = (1, 1);
@@ -235,24 +283,40 @@ where
 ///
 ///     let Positions: Vec<Position<usize>> = tuples.into_iter().convert();
 ///
-///     assert_eq!(vec![Position::new(0, 0), Position::new(1, 1), Position::new(2, 2)], Positions)
+///     assert_eq!(vec![Position::new(0, 0), Position::new(1, 1), Position::new(2, 2)], Positions);
+/// 
+///     let tups = vec![(4, 4), (5, 5), (3, 4)];
+/// 
+///     let mut convert_iter = tups.into_iter().rev().convert_iter();
+/// 
+///     assert_eq!(Some(Position::new(3, 4)), convert_iter.next());
 /// }
 /// ```
-pub trait ToVec<T, U, I>
+pub trait Convert<T, U, I>
 where
     U: From<T>,
     I: Iterator<Item = T>,
 {
+    /// Returns a vector of all completed conversions.
     fn convert(self) -> Vec<U>;
+    
+    /// Returns an iterator that performs the conversions.
+    fn convert_iter(self) -> std::iter::Map<I, fn(T) -> U>;
 }
 
-impl<T, U, I> ToVec<T, U, I> for I
+impl<T, U, I> Convert<T, U, I> for I
 where
     U: From<T>,
     I: Iterator<Item = T>,
 {
+    #[inline]
     fn convert(self) -> Vec<U> {
-        self.map(|item| U::from(item)).collect()
+        self.convert_iter().collect()
+    }
+
+    #[inline]
+    fn convert_iter(self) -> std::iter::Map<I, fn(T) -> U> {
+        self.map(|item| U::from(item))
     }
 }
 
