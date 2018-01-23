dd29d0de4b7a0737fd96a81be29c0ead6745651a
DutchGhost <kasper199914@gmail.com>
2018-01-23 22:19:32 +0100

    added an errortype for the collectarray!() macro

--- src/convert.rs
+++ src/convert.rs
@@ -5,7 +5,7 @@ use ::std::str::FromStr;
 /// # Examples
 /// ```
 /// extern crate libaoc;
-/// 
+///
 /// use libaoc::convert::TryConvert;
 ///
 /// fn main () {
@@ -20,10 +20,10 @@ use ::std::str::FromStr;
 ///
 ///     assert_eq!(Some(Ok(1)), iter.next());
 ///     assert_eq!(Some(Ok(2)), iter.next());
-/// 
+///
 ///     //the next would be 3, but that gives an error. assert_NE.
 ///     assert_ne!(Some(Ok(3)), iter.next());
-/// 
+///
 ///     //due to the split call, 4 isn't part of the stream, so next one is 5.
 ///     assert_eq!(Some(Ok(5)), iter.next());
 /// }
@@ -46,31 +46,31 @@ where
     /// Tries to convert a stream of T into a slice of U.
     /// On an error, returns how many items where converted.
     /// # Examples
-    /// 
+    ///
     /// ```
     /// extern crate libaoc;
-    /// 
+    ///
     /// use libaoc::convert::TryConvert;
-    /// 
+    ///
     /// fn main() {
     ///     let s = "1, 2, 3, 4, 5,6";
     ///     let mut buff = [0i64; 6];
-    ///     
+    ///
     ///     let succeded = s.split(", ").try_convert_into_slice(&mut buff);
-    /// 
+    ///
     ///     assert_eq!([1, 2, 3, 4, 0, 0], buff);
     ///     assert_eq!(Err(4), succeded);
-    /// 
+    ///
     ///     let s = "1, 2, 3, 4, 5, 6, 7, 8, 9";
     ///     let written = s.split(", ").try_convert_into_slice(&mut buff);
     ///     assert_eq!(Ok(6), written);
-    /// 
+    ///
     ///     let written = s.split(", ").take(2).try_convert_into_slice(&mut buff);
     ///     assert_eq!(Ok(2), written);
     /// }
     /// ```
     fn try_convert_into_slice(self, slice: &mut [U]) -> Result<usize, usize>;
-    
+
     /// Returns an iterator over the converted items. Returns an error if an item can not be converted. Continue's after the error.
     fn try_convert_iter(self) -> Self::Iterable;
 }
@@ -80,7 +80,7 @@ where
     U: FromStr,
     S: AsRef<str>,
     I: Iterator<Item = S>,
-{   
+{
     type Error = <U as FromStr>::Err;
     type Iterable = ::std::iter::Map<I, fn(S) -> Result<U, Self::Error>>;
 
@@ -112,14 +112,14 @@ where
 
 
 /// This trait allows to convert a stream with items of type T into a stream or collection with items of type U.
-/// 
+///
 /// # Examples
 /// ```
 /// extern crate libaoc;
-/// 
+///
 /// use libaoc::convert::Convert;
 /// use libaoc::movement::Position;
-/// 
+///
 /// fn main() {
 ///     let tuple1 = (0, 0);
 ///     let tuple2 = (1, 1);
@@ -130,11 +130,11 @@ where
 ///     let Positions: Vec<Position<usize>> = tuples.into_iter().convert();
 ///
 ///     assert_eq!(vec![Position::new(0, 0), Position::new(1, 1), Position::new(2, 2)], Positions);
-/// 
+///
 ///     let tups = vec![(4, 4), (5, 5), (3, 4)];
-/// 
+///
 ///     let mut convert_iter = tups.into_iter().rev().convert_iter();
-/// 
+///
 ///     assert_eq!(Some(Position::new(3, 4)), convert_iter.next());
 /// }
 /// ```
@@ -145,30 +145,30 @@ where
 {
     /// The Iterator that gets returned from [convert_iter()](trait.Convert.html#tymethod.convert_iter)
     type Iterable: Iterator<Item = U>;
-    
+
     /// Returns a vector of all completed conversions.
     fn convert(self) -> Vec<U>;
 
     /// Converts the stream, and writes the items into `slice`. Returns how many elements where written to the slice.
-    /// 
+    ///
     /// # Examples
     /// ```
     /// extern crate libaoc;
-    /// 
+    ///
     /// use libaoc::convert::Convert;
-    /// 
+    ///
     /// fn main() {
     ///     let chars = vec![97, 98, 99, 100, 101];
     ///     let mut slice: [char; 5] = ['-'; 5];
-    /// 
+    ///
     ///     let written = chars.into_iter().convert_into_slice(&mut slice);
-    ///     
+    ///
     ///     assert_eq!(['a', 'b', 'c', 'd', 'e'], slice);
     ///     assert_eq!(5, written);
     /// }
     /// ```
     fn convert_into_slice(self, slice: &mut [U]) -> usize;
-    
+
     /// Returns an iterator that performs the conversions.
     fn convert_iter(self) -> Self::Iterable;
 }
@@ -184,7 +184,7 @@ where
     fn convert(self) -> Vec<U> {
         self.convert_iter().collect()
     }
-    
+
     #[inline]
     fn convert_into_slice(self, slice: &mut [U]) -> usize {
         slice
@@ -193,15 +193,16 @@ where
             .map(|(dst, src)| *dst = src)
             .count()
     }
-    
+
     #[inline]
     fn convert_iter(self) -> Self::Iterable {
         self.map(move |item| U::from(item))
     }
 }
+
 /// This macro makes it easy to convert an Iterator into an array.
 /// The `type` of the array has to be specified when this macro is called.
-/// 
+///
 /// The array that get's build uses mem::unitialized to prevent unnecessary allocation,
 /// however if the Iterator has less items than the lenght of the array, this means there is still
 /// unitialized memory. In this case, the macro will return an error, and drop the array that was build.
@@ -209,10 +210,10 @@ where
 /// ```
 /// #[macro_use]
 /// extern crate libaoc;
-/// 
+///
 /// use libaoc::movement::Position;
 /// use libaoc::convert::Convert;
-/// 
+///
 /// #[derive(Debug, PartialEq)]
 /// struct noncopy{item: i64}
 /// impl From<i64> for noncopy {
@@ -221,17 +222,41 @@ where
 ///     }
 /// }
 /// fn main() {
-/// 
+///
 ///     let ns = vec![1, 2, 3];
-/// 
+///
 ///     let arr = arraycollect!(ns.into_iter().convert_iter() => [noncopy; 2]);
 ///     assert_eq!(Ok([noncopy{item: 1}, noncopy{item: 2}]), arr);
 /// }
 /// ```
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub enum FillError {
+    FillError,
+}
+
+impl ::std::error::Error for FillError {
+
+    #[inline]
+    fn description(&self) -> &str {
+        match self {
+            &FillError::FillError => "The array was partially ({} of {} items) filled, and therefore dropped."
+        }
+    }
+}
+
+impl ::std::fmt::Display for FillError {
+
+    #[inline]
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
 #[macro_export]
 macro_rules! arraycollect {
     ($iter:expr => [$tgt:ty; $num:tt]) => (
-        {   
+        {
             use ::std::mem;
 
             struct PartialArray {
@@ -251,7 +276,7 @@ macro_rules! arraycollect {
                 }
 
                 #[inline]
-                fn fill_array<I: Iterator<Item = $tgt>>(mut self, iter: I) -> Result<[$tgt; $num], &'static str>
+                fn fill_array<I: Iterator<Item = $tgt>>(mut self, iter: I) -> Result<[$tgt; $num], $crate::convert::FillError>
                 {
                     for (dst, src) in self.data.iter_mut().zip(iter) {
                         unsafe {
@@ -263,7 +288,7 @@ macro_rules! arraycollect {
                     //if the number of items filled is not equal to the number of items that should have been written,
                     //return an error.
                     if self.fill != $num {
-                        Err("Not enough items")
+                        Err($crate::convert::FillError::FillError)
                     }
                     else {
                         Ok(self.finish())
